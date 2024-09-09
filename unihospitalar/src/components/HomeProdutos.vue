7e209bef7c5513eedc9834f7d6ed0262317bf40e
Ecolebrilha <144856681+Ecolebrilha@users.noreply.github.com>
2024-09-09 00:52:00 -0300

    Transição adicionada na tela de produtos

--- unihospitalar/src/components/HomeProdutos.vue
+++ unihospitalar/src/components/HomeProdutos.vue
@@ -412,6 +412,12 @@ main {
   padding: 30px;
   width: 250px;
   text-align: center;
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+}
+
+.card:hover {
+  transform: translateY(-5px);
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.15);
 }
 
 .card-icon {
