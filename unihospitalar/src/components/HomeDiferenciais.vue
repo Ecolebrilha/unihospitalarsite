0456206cfd0680cc1bab31ff7754b5eed5413e14
Ecolebrilha <144856681+Ecolebrilha@users.noreply.github.com>
2024-09-10 12:18:50 -0300

    Update HomeDiferenciais.vue

--- unihospitalar/src/components/HomeDiferenciais.vue
+++ unihospitalar/src/components/HomeDiferenciais.vue
@@ -327,7 +327,7 @@ header {
 }
 
 .custom-select {
-  margin-bottom: 5px;
+  margin-bottom: 10px;
 }
 
 .icon-selector {
@@ -536,7 +536,7 @@ nav ul li.dropdown:hover .dropdown-content {
 .banner-container {
   position: relative;
   padding-top: 107px;
-  margin-top: -60px;
+  margin-top: -62px;
   display: flex;
   justify-content: center;
   background-color: #F9F9F9;
