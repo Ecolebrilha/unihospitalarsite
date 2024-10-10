ba5fbaa81cce8d87cd49f95196011e1519bcb342
Ecolebrilha <fernandobastosleite7@gmail.com>
2024-10-10 10:30:13 -0300

    Teste de rota

--- unihospitalar/src/components/HomeHeader.vue
+++ unihospitalar/src/components/HomeHeader.vue
@@ -26,7 +26,7 @@
       </ul>
     </div>
 
-    <a href="/">
+    <a href="unihospitalarsite/">
       <div class="logo-container">
         <img src="@/assets/logo-uni.png" alt="Logo da Uni Hospitalar" class="logo">
       </div>
